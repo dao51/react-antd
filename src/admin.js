1c5a52b660252312107f793b669fbceb8419be28
dao51 <fsd5816@163.com>
2020-12-21 18:45:50 +0800

    基本样式修改

--- src/admin.js
+++ src/admin.js
@@ -3,19 +3,20 @@ import {Row, Col} from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import './styles/common.less'
 
 export default class Admin extends React.Component {  // 导出继承react组件的对象Admin
     render() { // react生命周期方法
         return (  // return 根节点 有且只有一个
             <div>
-                <Row>
-                    <Col span="3">
+                <Row className="container"> 
+                    <Col span="3" className="nav-left">
                         <NavLeft></NavLeft>
                     </Col>
-                    <Col span="21">
+                    <Col span="21" className="main">
                         <Header>
                         </Header>
-                        <Row>content</Row>
+                        <Row className="content">content</Row>
                         <Footer>
                         </Footer>
                     </Col>
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -2,8 +2,12 @@ import React from 'react'  // 导入react
 
 export default class NavLeft extends React.Component {
     render() {
+        // var redWord = {
+        //     color: 'red'
+        // }
+        // 还可在行内设置<div style={redWord}>
         return (
-            <div>
+            <div style={{ color: 'red' }}>
                 NavLeft
             </div>
         );
