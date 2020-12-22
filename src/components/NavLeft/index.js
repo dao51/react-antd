a9aadfd63d80df8eb9d786a3a2835c0962448fcb
dao51 <fsd5816@163.com>
2020-12-22 10:36:47 +0800

    菜单初步设置

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,4 +1,10 @@
-import React from 'react'  // 导入react
+import React from 'react';  // 导入react
+import { Menu } from 'antd';  // 导入菜单组件
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';  // 导入icon
+import MenuConfig from './../../config/menuConfig';  // 导入菜单路由文件
+import './index.less';
+
+const { SubMenu } = Menu;
 
 export default class NavLeft extends React.Component {
     render() {
@@ -8,7 +14,20 @@ export default class NavLeft extends React.Component {
         // 还可在行内设置<div style={redWord}>
         return (
             <div style={{ color: 'red' }}>
-                NavLeft
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt="" />
+                    <h1>IReact</h1>
+                </div>
+                <Menu
+                    theme="dark"
+                >
+                    <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </SubMenu>
+                </Menu>
             </div>
         );
     }
