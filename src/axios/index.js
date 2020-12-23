f4cede8138a6fe4b86eb13588bb244b5ede983a2
dao51 <fsd5816@163.com>
2020-12-23 11:00:47 +0800

    头部功能完成

---
+++ src/axios/index.js
@@ -0,0 +1,18 @@
+import JsonP from 'jsonp'
+
+export default class Axios{
+    // 封装jsonp插件
+    static jsonp(options){
+        new Promise((resolve, reject) => {
+            JsonP(options.url,{
+                param:'callback'
+            }, function (err, response) {
+                if(response.status === 'success'){
+                    resolve(response);
+                }else{
+                    reject(response.message);
+                }
+            })
+        })
+    }
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
-import React from 'react'  // 导入react
+import React from 'react';  // 导入react
 import { Row, Col } from 'antd';  // 导入菜单组件
 import './index.less';
 import Utils from './../../utils/utils';
+import axios from '../../axios';
 
 export default class Header extends React.Component {
     constructor(props) {  // 构造函数
@@ -10,6 +11,8 @@ export default class Header extends React.Component {
         this.state = { // 初始数据
             userName: '',
             sysTime: '',
+            dayPictureUrl: '',
+            weather: '',
         }
     }
 
@@ -24,6 +27,23 @@ export default class Header extends React.Component {
                 sysTime: sysTime,
             })
         }, 1000);
+        // this.getWeatherAPIDate();  // 百度天气api变更后，原有功能无法使用
+    }
+    // 获取天气API数据
+    getWeatherAPIDate() {
+        let city = encodeURIComponent('上海');
+        axios.jsonp({
+            url: 'http://api.map.baidu.com/telematics/v3/weather?location='+city+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        }).then((res) => {
+            if(res.status === 'success'){
+                let data = res.result[0].weather_data[0];
+                console.log('weather', data)
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
+            } 
+        })
     }
 
     render() {
@@ -41,7 +61,10 @@ export default class Header extends React.Component {
                     </Col>
                     <Col span="20" className="weather">
                         <span className="date">{this.state.sysTime}</span>
-                        <span className="weather-detail">晴转多云</span>
+                        <span className="weather-img">
+                            <img src={this.state.dayPictureUrl} alt="" />
+                        </span>
+                        <span className="weather-detail">{this.state.weather}</span>
                     </Col>
                 </Row>
             </div>
